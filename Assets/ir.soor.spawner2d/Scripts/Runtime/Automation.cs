415ffef654251abe809ed7a5f374796dfd19e8b4
abolfazl hosnian <abolfazlhosnian@gmail.com>
2025-08-01 19:47:43 +0330

    Automation-Comments added

--- Assets/ir.soor.spawner2d/Scripts/Runtime/Automation.cs
+++ Assets/ir.soor.spawner2d/Scripts/Runtime/Automation.cs
@@ -4,27 +4,75 @@ using UnityEngine.Events;
 
 namespace Soor.Spawner2d
 {
+    /// <summary>
+    /// Configures the automated spawning behavior of the spawner.
+    /// Manages properties like spawn interval, and connects to limitation settings.
+    /// </summary>
     [Serializable]
     public class Automation
     {
+        #region SERIALIZED_FIELDS
+
+        /// <summary>
+        /// The delay in seconds between two consecutive spawns.
+        /// </summary>
         [Tooltip("Delay between two consecutive spawns in milliseconds.")]
         [SerializeField] private float _perSpawnInterval;
+        
+        /// <summary>
+        /// When true, the spawner will automatically stop spawning based on the defined limitations.
+        /// </summary>
         [SerializeField] private bool _stopSpawningAutomatically;
+        
+        /// <summary>
+        /// The settings used to define and manage the spawning limitations (e.g., max count, max time).
+        /// This is only used when <see cref="_stopSpawningAutomatically"/> is enabled.
+        /// </summary>
         [SerializeField] private Limitation _limitationSettings;
+
+        #endregion SERIALIZED_FIELDS
+        
 
         #region EVENTS
 
+        /// <summary>
+        /// Event triggered at the beginning of the automated spawning process.
+        /// </summary>
         public UnityEvent onSpawnStartEvent = new UnityEvent();
+        
+        /// <summary>
+        /// Event triggered when the automated spawning process ends.
+        /// </summary>
         public UnityEvent onSpawnEndEvent = new UnityEvent();
 
         #endregion EVENTS
-        
-        
 
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// Indicates whether the spawner should automatically stop based on limitations.
+        /// </summary>
         public bool StopSpawningAutomatically => _stopSpawningAutomatically;
+        
+        /// <summary>
+        /// The time interval between each spawn.
+        /// </summary>
         public float PerSpawnInterval => _perSpawnInterval;
+        
+        /// <summary>
+        /// Provides access to the limitation settings.
+        /// </summary>
         public Limitation LimitationSettings => _limitationSettings;
 
+        #endregion PROPERTIES
+
+
+        #region METHODS
+
+        /// <summary>
+        /// Initiates the spawning process, triggering the start event and the limitation setup if needed.
+        /// </summary>
         public void OnSpawnStart()
         {
             onSpawnStartEvent?.Invoke();
@@ -35,6 +83,9 @@ namespace Soor.Spawner2d
             }
         }
     
+        /// <summary>
+        /// Finalizes the spawning process, triggering the end event.
+        /// </summary>
         public void OnSpawnEnd()
         {
             if (_stopSpawningAutomatically)
@@ -46,5 +97,7 @@ namespace Soor.Spawner2d
                 onSpawnEndEvent?.Invoke();
             }
         }
+
+        #endregion METHODS
     }
 }
