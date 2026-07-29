b809551f27871e9f232ab19907f8d933ecdc3d60
abolfazl hosnian <abolfazlhosnian@gmail.com>
2026-07-29 14:58:00 +0330

    TestScript_2-namespace fixed

--- Assets/ir.soor.spawner2d/Test/TestScript_2.cs
+++ Assets/ir.soor.spawner2d/Test/TestScript_2.cs
@@ -1,25 +1,28 @@
 using Soor.Spawner2d;
 using UnityEngine;
 
-public class TestScript_2 : MonoBehaviour
+namespace Test
 {
-    [SerializeField] private Spawner _spawner;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class TestScript_2 : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) ||
-            Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) ||
-            Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl) ||
-            Input.GetKeyDown(KeyCode.RightCommand) || Input.GetKeyDown(KeyCode.LeftCommand) ||
-            Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt))
+        [SerializeField] private Spawner _spawner;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            _spawner.Spawn();
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) ||
+                Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) ||
+                Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl) ||
+                Input.GetKeyDown(KeyCode.RightCommand) || Input.GetKeyDown(KeyCode.LeftCommand) ||
+                Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt))
+            {
+                _spawner.Spawn();
+            }
         }
     }
 }
