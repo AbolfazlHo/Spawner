32c586f6be7c0bf2ff76474c3eef539f20c6c68b
abolfazl hosnian <abolfazlhosnian@gmail.com>
2026-07-29 14:26:34 +0330

    Differnet demo scenes added

---
+++ Assets/ir.soor.spawner2d/Test/TestScript_2.cs
@@ -0,0 +1,25 @@
+using Soor.Spawner2d;
+using UnityEngine;
+
+public class TestScript_2 : MonoBehaviour
+{
+    [SerializeField] private Spawner _spawner;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) ||
+            Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) ||
+            Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl) ||
+            Input.GetKeyDown(KeyCode.RightCommand) || Input.GetKeyDown(KeyCode.LeftCommand) ||
+            Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            _spawner.Spawn();
+        }
+    }
+}
